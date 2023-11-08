d0fc57a3334979b24cc047aa21388a2c366f2ad9
Vijay Balasubramaniam <vbalasu@gmail.com>
2023-11-07 21:25:26 -0800

    Now works in the browser

--- README.md
+++ README.md
@@ -17,3 +17,10 @@ USAGE: curl "http://127.0.0.1:8000/sheets?spreadsheet_id=1pYY4glskbi8RMXXPaH4SGJ
               curl "https://get.cloudmatica.com/sheets?spreadsheet_id=1pYY4glskbi8RMXXPaH4SGJI5O-J_tXzAq1uImt2siv4&sheet_name=customers" 
               assuming the spreadsheet has been shared with sheets@cloudmatica.iam.gserviceaccount.com
 ```
+
+Use the same Javascript in NodeJS and the browser by using a conditional require as follows:
+```
+if(typeof window == 'undefined') var axios = require('axios');   // Applies only to Node.js. Use CDN in index.html
+```
+
+[index.html](./index.html)
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+if(typeof window == 'undefined') var axios = require('axios');   // Applies only to Node.js. Use CDN in index.html
+
 async function readSpreadsheet(spreadsheetId, sheetName) {
-    const axios = require('axios');
     var response = await axios.get(`https://get.cloudmatica.com/sheets?spreadsheet_id=${spreadsheetId}&sheet_name=${sheetName}`);
     console.log(response);
+    return response;
+}
+
+async function readPage(url) {
+    var response = await axios.get(`https://get.cloudmatica.com?url=${url}`);
+    console.log(response);
+    return response;
 }
 
 readSpreadsheet('1pYY4glskbi8RMXXPaH4SGJI5O-J_tXzAq1uImt2siv4', 'customers');
+//readPage('https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population');
